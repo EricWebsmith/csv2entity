ab6fc023d78cb793a35f97bd81b2e9ed38101e76
Eric Zhou <zhouweiguang@hotmail.com>
2019-01-12 23:52:13 +0800

    convert the project from vs2010 to vs2017. a lot of changes.

---
+++ Backup/Templates/Ezfx.Csv.Templates.CsvClass.CS/Csv.cs
@@ -0,0 +1,11 @@
+﻿using Ezfx.Csv;
+
+namespace $rootnamespace$
+{
+
+    [CsvObject(CodePage = $codepage$, Description = "", HasHeader = true, Name = "$tableName$",MappingType=CsvMappingType.$MappingType$, Delimiter = "$Delimiter$")]
+    public class $safeitemrootname$
+	{
+        $properties$
+	}
+}
---
+++ Templates/Ezfx.Csv.Templates/Wizard/ConfigForm.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Ezfx.Csv.ItemWizards
+{
+    public partial class ConfigForm : Form
+    {
+
+        public CsvConfig Config { get; set; }
+
+        public ConfigForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ConfigForm_Load(object sender, EventArgs e)
+        {
+            Config = new CsvConfig();
+            this.csvConfigBindingSource.DataSource = Config;
+            Config.PropertyChanged += new System.ComponentModel.PropertyChangedEventHandler(Config_PropertyChanged);
+            LoadEncoding();
+        }
+
+        void Config_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            Display();
+        }
+
+        private void okayButton_Click(object sender, EventArgs e)
+        {
+            Display();
+            this.Hide();
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            BrowseFile();
+        }
+
+        private void BrowseFile()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "CSV Files (*.csv)|*.csv|Access Files(*.mdb;*.accdb)|*.mdb;*.accdb|Excel Files (*.xls;*.xlsx)|*.xls;*.xlsx|All Files (*.*)|*.*";
+            if (DialogResult.OK == ofd.ShowDialog())
+            {
+                pathTextBox.Text = ofd.FileName;
+                if (ofd.FileName.GetIsOleDb())
+                {
+                    foreach (string tableName in CsvContext.GetTableNames(ofd.FileName))
+                    {
+                        tableComboBox.Items.Add(tableName);
+                    }
+                    if (tableComboBox.Items.Count > 0)
+                    {
+                        tableComboBox.SelectedIndex = 0;
+                        return;
+                    }
+                }
+                Display();
+            }
+        }
+
+        #region Encoding
+
+        private void LoadEncoding()
+        {
+            Config.CodePage = Encoding.Default.CodePage;
+            List<EncodingInfo> infos = Encoding.GetEncodings().ToList();
+            infos.Sort((a, b) => a.DisplayName.CompareTo(b.DisplayName));
+            foreach (EncodingInfo info in infos)
+            {
+                encodingComboBox.Items.Add( info.DisplayName);
+            }
+            encodingComboBox.Text = Encoding.Default.EncodingName;
+        }
+
+        private void systemRadioButton_Click(object sender, EventArgs e)
+        {
+            Config.CodePage = Encoding.Default.CodePage;
+            Display();
+        }
+
+        private void unicodeRadioButton_Click(object sender, EventArgs e)
+        {
+            Config.CodePage = Encoding.Unicode.CodePage;
+            Display();
+        }
+
+        //private void encodingComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        //{
+        //    //Config.CodePage = ((MyEncoding)encodingComboBox.SelectedItem).CodePage;
+        //    Display();
+        //}
+
+        #endregion
+
+        private void Display()
+        {
+            if (pathTextBox.Text == string.Empty)
+            {
+                return;
+            }
+            DataTable dt = null;
+            if (pathTextBox.Text.GetIsOleDb())
+            {
+                if (tableComboBox.SelectedItem == null)
+                {
+                    return;
+                }
+                dt = CsvContext.GetDataTable(pathTextBox.Text, tableComboBox.SelectedItem.ToString());
+            }
+            else
+            {
+                dt = CsvContext.GetDataTable(pathTextBox.Text,Config.Delimiter, Encoding.GetEncoding(Config.CodePage));
+
+            }
+            Config.Columns.Clear();
+            for (int i = 0; i < dt.Columns.Count; i++)
+            {
+                Config.Columns.Add(new CsvColumn { Ordinal = i, Name = dt.Columns[i].ColumnName });
+            }
+            mainDataGridView.DataSource = dt;
+            this.Refresh();
+        }
+
+        private void tableComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Config.Name = tableComboBox.SelectedItem.ToString();
+            Display();
+        }
+
+    }
+}
