2b980f6b709b2951eebcd6a42c21914cfac12152
Eric Zhou <zhouweiguang@hotmail.com>
2019-01-17 08:36:48 +0800

    delete useless backup files created by upgrading from vs2015 to vs2017. this prevents building.

--- Backup/Templates/Ezfx.Csv.Templates.CsvClass.VB/Csv.vb
+++
@@ -1,8 +0,0 @@
-﻿Imports Ezfx.Csv
-
-<CsvObject(CodePage:=$codepage$, Description:="", HasHeader:=True, MappingType:=Ezfx.Csv.CsvMappingType.$MappingType$, Name:="$tableName$", Delimiter:="$Delimiter$")> _
-Public Class $safeitemname$
-
-$properties$
-
-End Class
